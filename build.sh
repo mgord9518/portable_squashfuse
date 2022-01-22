070c82e026e58d917065eec2f41cfd1fed1a004e
Mathew Gordon <mgord9518@gmail.com>
2022-01-22 13:53:28 +0000

    Add different compression methods

--- build.sh
+++ build.sh
@@ -16,11 +16,53 @@ ln -s /usr/lib/*/liblzma.a static/lib
 ln -s /usr/lib/*/libzstd.a static/lib
 
 ./autogen.sh
+
+# All supported compression methods
 ./configure --disable-shared --with-lz4=./static --with-xz=./static --with-zstd=./static
 make
 
 strip -s squashfuse squashfuse_extract squashfuse_ll squashfuse_ls
-mv squashfuse ../squashfuse_$ARCH
-mv squashfuse_extract ../squashfuse_extract_$ARCH
-mv squashfuse_ll ../squashfuse_ll_$ARCH
-mv squashfuse_ls ../squashfuse_ls_$ARCH
+mv squashfuse ../squashfuse_lz4_xz_zstd.$ARCH
+mv squashfuse_extract ../squashfuse_extract_lz4_xz_zstd.$ARCH
+mv squashfuse_ll ../squashfuse_ll_lz4_xz_zstd.$ARCH
+mv squashfuse_ls ../squashfuse_ls_lz4_xz_zstd.$ARCH
+
+# ZLIB, LZ4 and XZ
+./configure --disable-shared --with-lz4=./static --with-xz=./static --without-zstd
+make
+
+strip -s squashfuse squashfuse_extract squashfuse_ll squashfuse_ls
+mv squashfuse ../squashfuse_lz4_xz.$ARCH
+mv squashfuse_extract ../squashfuse_extract_lz4_xz.$ARCH
+mv squashfuse_ll ../squashfuse_ll_lz4_xz.$ARCH
+mv squashfuse_ls ../squashfuse_ls_lz4_xz.$ARCH
+
+# ZLIB, LZ4
+./configure --disable-shared --with-lz4=./static --without-xz --without-zstd
+make
+
+strip -s squashfuse squashfuse_extract squashfuse_ll squashfuse_ls
+mv squashfuse ../squashfuse_lz4.$ARCH
+mv squashfuse_extract ../squashfuse_extract_lz4.$ARCH
+mv squashfuse_ll ../squashfuse_ll_lz4.$ARCH
+mv squashfuse_ls ../squashfuse_ls_lz4.$ARCH
+
+# ZLIB, XZ
+./configure --disable-shared --without-lz4 --with-xz=./static --without-zstd
+make
+
+strip -s squashfuse squashfuse_extract squashfuse_ll squashfuse_ls
+mv squashfuse ../squashfuse_xz.$ARCH
+mv squashfuse_extract ../squashfuse_extract_xz.$ARCH
+mv squashfuse_ll ../squashfuse_ll_xz.$ARCH
+mv squashfuse_ls ../squashfuse_ls_xz.$ARCH
+
+# ZLIB, ZSTD
+./configure --disable-shared --without-lz4 --without-xz --with-zstd=./static
+make
+
+strip -s squashfuse squashfuse_extract squashfuse_ll squashfuse_ls
+mv squashfuse ../squashfuse_zstd.$ARCH
+mv squashfuse_extract ../squashfuse_extract_zstd.$ARCH
+mv squashfuse_ll ../squashfuse_ll_zstd.$ARCH
+mv squashfuse_ls ../squashfuse_ls_zstd.$ARCH
