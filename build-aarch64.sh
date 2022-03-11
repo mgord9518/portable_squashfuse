eef01daf43ecb88421f48f00f4225f218402d645
Mathew Gordon <mgord9518@gmail.com>
2022-03-11 12:37:46 +0000

    aarch64 test

---
+++ build-aarch64.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt install qemu-arm-static
+wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-arm64.squashfs
+mkdir -p chroot/tmp chroot/dev chroot/proc chroot/sys sfsmnt upper/usr/bin work
+cp /usr/bin/qemu-aarch64-static upper/usr/bin
+sudo mount -t squashfs bionic-server-cloudimg-arm64.squashfs sfsmnt
+sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chroot
+
+sudo mount -t proc proc chroot/proc/
+sudo mount -t sysfs sys chroot/sys/
+sudo mount -o bind /tmp chroot/tmp/
+sudo mount -o bind /dev chroot/dev/
+sudo mount --rbind /run/systemd chroot/run/systemd
+
+sudo cat << EOF | chroot chroot
+wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build.sh
+sh build.sh
+EOF
