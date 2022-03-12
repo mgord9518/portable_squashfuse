44146535cd724d403fde7c95462ec98eff7acb54
Mathew Gordon <mgord9518@gmail.com>
2022-03-12 02:56:43 +0000

    fix mkdir cmd

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -7,7 +7,7 @@ sudo update-binfmts --display qemu-arm
 sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register
 
 wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-arm64.squashfs
-mkdir -p chroot/tmp chroot/dev chroot/proc chroot/sys sfsmnt upper/usr/bin work chroot/run/systemd
+mkdir -p chroot/tmp chroot/dev chroot/proc chroot/sys sfsmnt upper/usr/bin work upper/run/systemd
 cp /usr/bin/qemu-aarch64-static upper/usr/bin
 sudo mount -t squashfs bionic-server-cloudimg-arm64.squashfs sfsmnt
 sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chroot
