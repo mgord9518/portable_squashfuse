c35b2c4f6c74e6e25bdd4cb471275f2b34da5781
Mathew Gordon <mgord9518@gmail.com>
2022-03-13 14:15:21 +0000

    clean up build

---
+++ build_in_chroot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+sudo apt install qemu-user-static
+
+mkdir -p chroot/tmp chroot/dev chroot/proc chroot/sys sfsmnt upper/usr/bin work upper/run/systemd
+
+# Move QEMU to the chroot directory
+cp /usr/bin/qemu-aarch64-static upper/usr/bin
+
+# Mount up the chroot
+sudo mount -t squashfs "bionic-server-cloudimg-$1.squashfs" sfsmnt
+sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chrootdir
+
+sudo mount -o bind /proc chrootdir/proc/
+#sudo mount -t sysfs sys chrootdir/sys/
+#sudo mount -o bind /tmp chrootdir/tmp/
+sudo mount -o bind /dev chrootdir/dev/
+sudo mount --rbind /run/systemd chrootdir/run/systemd
+
+# Everything below will be run inside the chroot
+cat << EOF | sudo chroot chrootdir /bin/bash
+wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build.sh
+sh build.sh
+EOF
