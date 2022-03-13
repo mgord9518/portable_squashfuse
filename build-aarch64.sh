48e3a91f41bbf712fcbc9c0826419f121ee38e5a
Mathew Gordon <mgord9518@gmail.com>
2022-03-13 13:55:31 +0000

    remove unneeded lines

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 sudo apt install qemu-user-static binfmt-support
-sudo update-binfmts --enable qemu-arm
-sudo update-binfmts --display qemu-arm
-#sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
-sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register
+#sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x27\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register
 
 wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-arm64.squashfs
 mkdir -p chroot/tmp chroot/dev chroot/proc chroot/sys sfsmnt upper/usr/bin work upper/run/systemd
@@ -19,10 +16,6 @@ sudo mount -o bind /tmp chroot/tmp/
 sudo mount -o bind /dev chroot/dev/
 sudo mount --rbind /run/systemd chroot/run/systemd
 ls
-
-mount
-#ls /run
-#ls $XDG_RUNTIME_DIR
 
 cat << EOF | sudo chroot chroot /bin/bash
 wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build.sh
