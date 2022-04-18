f535a69b4f9989166e7e891a66cd52e4772afab2
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-04-18 09:34:48 +0000

    Update build_in_chroot.sh

--- build_in_chroot.sh
+++ build_in_chroot.sh
@@ -2,20 +2,22 @@
 
 sudo apt install qemu-user-static
 
-mkdir -p chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
+mkdir $1 && cd $1
+
+mkdir -p chrootdir1/tmp chrootdir1/dev chrootdir1/proc sfsmnt1 upper1/usr/bin work1 upper1/run/systemd
 
 # Move QEMU to the chroot directory
-cp /usr/bin/qemu-aarch64-static upper/usr/bin
+cp /usr/bin/qemu-aarch64-static upper1/usr/bin
 
 # Mount up the chroot
-sudo mount -t squashfs "impish-server-cloudimg-$1.squashfs" sfsmnt
-sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chrootdir
+sudo mount -t squashfs "impish-server-cloudimg-$1.squashfs" sfsmnt$1
+sudo mount -t overlay overlay -olowerdir=sfsmnt1,upperdir=upper1,workdir=work1 chrootdir1
 
-sudo mount -o bind /proc chrootdir/proc/
-sudo mount --rbind /run/systemd chrootdir/run/systemd
+sudo mount -o bind /proc chrootdir1/proc/
+sudo mount --rbind /run/systemd chrootdir1/run/systemd
 
 # --- INSIDE CHROOT ---
-cat << EOF | sudo chroot chrootdir /bin/bash
+cat << EOF | sudo chroot chrootdir1 /bin/bash
 sudo apt update
 sudo apt install -y zlib1g-dev liblzma-dev libzstd-dev liblz4-dev make gcc libfuse-dev libfuse3-dev autoconf libtool pkg-config
 
@@ -24,13 +26,13 @@ sh build-static.sh
 EOF
 # --- EXIT CHROOT ---
 
-cp chrootdir/squashfuse* ./
-sudo umount sfsmnt chrootdir/proc chrootdir/run/systemd chrootdir
-mount
+cp chrootdir1/squashfuse* ../
+#sudo umount sfsmnt chrootdir/proc chrootdir/run/systemd chrootdir
+#mount
 
 #exit 0
 
-sudo rm -rf chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
+#sudo rm -rf chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
 mkdir -p chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
 
  
@@ -54,6 +56,6 @@ sh build.sh
 EOF
 # --- EXIT CHROOT ---
 
-cp chrootdir/squashfuse* ./
-sudo umount sfsmnt chrootdir/proc chrootdir/run/systemd chrootdir
-sudo rm -rf chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
+cp chrootdir/squashfuse* ../
+#sudo umount sfsmnt chrootdir/proc chrootdir/run/systemd chrootdir
+#sudo rm -rf chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
