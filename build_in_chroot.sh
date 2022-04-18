d1af08956d5d7a55269c74032bf06a418d369745
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-04-18 08:33:54 +0000

    Update build_in_chroot.sh

--- build_in_chroot.sh
+++ build_in_chroot.sh
@@ -12,15 +12,16 @@ sudo mount -t squashfs "impish-server-cloudimg-$1.squashfs" sfsmnt
 sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chrootdir
 
 sudo mount -o bind /proc chrootdir/proc/
-#sudo mount -o bind /dev chrootdir/dev/
 sudo mount --rbind /run/systemd chrootdir/run/systemd
 
-# Everything below will be run inside the chroot
+# --- INSIDE CHROOT ---
 cat << EOF | sudo chroot chrootdir /bin/bash
 sudo apt update
-#sudo apt install -y zlib1g-dev liblzma-dev libzstd-dev liblz4-dev make gcc libfuse-dev autoconf libtool pkg-config
 sudo apt install -y zlib1g-dev liblzma-dev libzstd-dev liblz4-dev make gcc libfuse-dev libfuse3-dev autoconf libtool pkg-config
 
 wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build.sh
 sh build.sh
 EOF
+# --- EXIT CHROOT ---
+
+sudo umount sfsmnt
