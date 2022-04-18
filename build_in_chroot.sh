c8013ffe213412f0880fa477d29253d347c5bb40
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-04-18 08:39:27 +0000

    Update build_in_chroot.sh

--- build_in_chroot.sh
+++ build_in_chroot.sh
@@ -20,8 +20,28 @@ sudo apt update
 sudo apt install -y zlib1g-dev liblzma-dev libzstd-dev liblz4-dev make gcc libfuse-dev libfuse3-dev autoconf libtool pkg-config
 
 wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build.sh
-sh build.sh
+sh build-static.sh
 EOF
 # --- EXIT CHROOT ---
 
-sudo umount sfsmnt
+sudo umount sfsmnt chrootdir chrootdir/proc chrootdir/run/systemd
+
+
+
+
+# Mount up the chroot
+sudo mount -t squashfs "bionic-server-cloudimg-$1.squashfs" sfsmnt
+sudo mount -t overlay overlay -olowerdir=sfsmnt,upperdir=upper,workdir=work chrootdir
+
+sudo mount -o bind /proc chrootdir/proc/
+sudo mount --rbind /run/systemd chrootdir/run/systemd
+
+# --- INSIDE CHROOT ---
+cat << EOF | sudo chroot chrootdir /bin/bash
+sudo apt update
+sudo apt install -y zlib1g-dev liblzma-dev libzstd-dev liblz4-dev make gcc libfuse-dev autoconf libtool pkg-config
+
+wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build.sh
+sh build.sh
+EOF
+# --- EXIT CHROOT ---
