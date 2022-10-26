f0488316de96470c02e91af203e51da29684e5f3
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-10-25 23:17:13 -0600

    Update build-static.sh

--- build-static.sh
+++ build-static.sh
@@ -1,31 +1,28 @@
-# TODO: clean up this entire script
-# MUST be run under Ubuntu 21.04 or newer
-# For some reason FUSE3 gets undefined references while static linking otherwise
+#!/bin/sh
+# Static build script designed for Alpine
 
 [ -z $ARCH ] && ARCH=$(uname -m)
 
 git clone https://github.com/vasi/squashfuse
 cd squashfuse
-mkdir -p static/lib
 
 ./autogen.sh
 
-# All supported compression methods
-./configure CFLAGS='-static -Os'
-make LDFLAGS='-all-static'
+export CFLAGS='-static -Os'
+export LDFLAGS='-all-static'
 
-cat config.log
+# All supported compression methods
+./configure
+make
 
 mv squashfuse ../squashfuse_lz4_xz_zstd-static.$ARCH
 mv squashfuse_extract ../squashfuse_extract_lz4_xz_zstd-static.$ARCH
 mv squashfuse_ll ../squashfuse_ll_lz4_xz_zstd-static.$ARCH
 mv squashfuse_ls ../squashfuse_ls_lz4_xz_zstd-static.$ARCH
 
-#!/bin/sh
-
 # ZLIB, LZ4 and XZ
-./configure CFLAGS='-static -Os' --without-zstd
-make LDFLAGS='-all-static'
+./configure --without-zstd
+make
 
 mv squashfuse ../squashfuse_lz4_xz-static.$ARCH
 mv squashfuse_extract ../squashfuse_extract_lz4_xz-static.$ARCH
@@ -33,8 +30,8 @@ mv squashfuse_ll ../squashfuse_ll_lz4_xz-static.$ARCH
 mv squashfuse_ls ../squashfuse_ls_lz4_xz-static.$ARCH
 
 # ZLIB, LZ4
-./configure CFLAGS='-static -Os' --without-zstd --without-xz
-make LDFLAGS='-all-static'
+./configure --without-zstd --without-xz
+make
 
 mv squashfuse ../squashfuse_lz4-static.$ARCH
 mv squashfuse_extract ../squashfuse_extract_lz4-static.$ARCH
@@ -42,8 +39,8 @@ mv squashfuse_ll ../squashfuse_ll_lz4-static.$ARCH
 mv squashfuse_ls ../squashfuse_ls_lz4-static.$ARCH
 
 # ZLIB, XZ
-./configure CFLAGS='-static -Os' --without-zstd --without-lz4
-make LDFLAGS='-all-static'
+./configure --without-zstd --without-lz4
+make
 
 mv squashfuse ../squashfuse_xz-static.$ARCH
 mv squashfuse_extract ../squashfuse_extract_xz-static.$ARCH
@@ -51,8 +48,8 @@ mv squashfuse_ll ../squashfuse_ll_xz-static.$ARCH
 mv squashfuse_ls ../squashfuse_ls_xz-static.$ARCH
 
 # ZLIB, ZSTD
-./configure CFLAGS='-static -Os' --without-xz --without-lz4
-make LDFLAGS='-all-static'
+./configure --without-xz --without-lz4
+make
 
 mv squashfuse ../squashfuse_zstd-static.$ARCH
 mv squashfuse_extract ../squashfuse_extract_zstd-static.$ARCH
