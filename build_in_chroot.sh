45f83136e972e461c35b5890e885a95ad5ee0d20
Mathew R Gordon <72643694+mgord9518@users.noreply.github.com>
2022-10-31 22:13:37 -0600

    Update build_in_chroot.sh

--- build_in_chroot.sh
+++ build_in_chroot.sh
@@ -18,11 +18,13 @@ sudo mount --rbind /run/systemd chrootdir1/run/systemd
 
 # --- INSIDE CHROOT ---
 cat << EOF | sudo chroot chrootdir1 /bin/bash
-sudo apt update
-sudo apt install -y zlib1g-dev liblzma-dev libzstd-dev liblz4-dev make gcc libfuse-dev libfuse3-dev autoconf libtool pkg-config
+
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh
+distrobox create --name alpine --image alpine:3.16 --yes
+distrobox enter alpine -- sudo apk add autoconf git gcc automake make libc-dev fuse3-dev fuse3-static libtool zlib-dev xz-dev zstd-dev lz4-dev zlib-static zstd-static lz4-static
 
 wget https://raw.githubusercontent.com/mgord9518/portable_squashfuse/main/build-static.sh
-sh build-static.sh
+distrobox enter alpine -- sh build-static.sh
 EOF
 # --- EXIT CHROOT ---
 
@@ -30,7 +32,7 @@ cp chrootdir1/squashfuse* ../
 #sudo umount sfsmnt chrootdir/proc chrootdir/run/systemd chrootdir
 #mount
 
-#exit 0
+exit 0
 
 #sudo rm -rf chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
 mkdir -p chrootdir/tmp chrootdir/dev chrootdir/proc sfsmnt upper/usr/bin work upper/run/systemd
